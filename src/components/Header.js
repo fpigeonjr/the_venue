fbf16e6864cbf7391be34aece7b4fa97211c99a8
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-10-03 01:58:51 -0500

    adds Header and SideDrawar components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import MenuIcon from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
+import SideDrawar from "./SideDrawar";
 
 class Header extends Component {
+  state = {
+    drawarOpen: false,
+    headerShow: false
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  handleScroll = () => {
+    if (window.scrollY > 0) {
+      this.setState({
+        headerShow: true
+      });
+    } else {
+      this.setState({
+        headerShow: false
+      });
+    }
+  };
+  toggleDrawar = value => {
+    this.setState({
+      drawarOpen: value
+    });
+  };
+
   render() {
     return (
       <AppBar
         position="fixed"
         style={{
-          backgroundColor: "#2f2f2f",
+          backgroundColor: this.state.headerShow ? "#2f2f2f" : "transparent",
           boxShadow: "none",
           padding: "10px 0 "
         }}
@@ -20,6 +46,17 @@ class Header extends Component {
             <div className="font_righteous header_logo_venue">The Venue</div>
             <div className="header_logo_title">Musical Events</div>
           </div>
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => this.toggleDrawar(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <SideDrawar
+            open={this.state.drawarOpen}
+            onClose={value => this.toggleDrawar(value)}
+          />
         </Toolbar>
       </AppBar>
     );
