9709517ad246faed9980ae850707d17ab9ca27fb
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-10-05 22:17:24 -0500

    Updates

---
+++ precache-manifest.1a612e0ca0c58ff38b65532384531bb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/the_venue/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/the_venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/the_venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "39afd44280ba6883126b",
+    "url": "/the_venue/static/js/runtime~main.39afd442.js"
+  },
+  {
+    "revision": "0a1103fa81389c76cbbd",
+    "url": "/the_venue/static/js/main.0a1103fa.chunk.js"
+  },
+  {
+    "revision": "5e444435ebe8960b02b5",
+    "url": "/the_venue/static/js/1.5e444435.chunk.js"
+  },
+  {
+    "revision": "0a1103fa81389c76cbbd",
+    "url": "/the_venue/static/css/main.291638fa.chunk.css"
+  },
+  {
+    "revision": "bd0aef32621cd2c2bfa4397a19bb6970",
+    "url": "/the_venue/index.html"
+  }
+];
